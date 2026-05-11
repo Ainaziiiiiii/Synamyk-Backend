9ecd5790b912f254364bf4fe4fa436385ea3fa06
Ainazik <ainazik@gmail.com>
2026-05-11 13:30:32 +0600

    init

--- src/main/java/synamyk/controller/AdminPaymentController.java
+++ src/main/java/synamyk/controller/AdminPaymentController.java
@@ -30,7 +30,7 @@ public class AdminPaymentController {
 
     private final AdminPaymentService adminPaymentService;
 
-    
+
     @GetMapping
     @Operation(
             summary = "Список платежей",
